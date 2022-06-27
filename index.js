4842ae7a4b492fd047072fadb618c90de1cadaff
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-06-27 15:41:36 -0300

    Corrige requisitos 1 e 2.

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-const talkers = require('./talker.json');
+const fs = require('fs/promises');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', (_req, res) => {
+app.get('/talker', async (_req, res) => {
+  const talkers = JSON.parse(await fs.readFile('./talker.json'));
   res.status(200).json(talkers);
 });
 
-app.get('/talker/:id', (req, res) => {
+app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
+  const talkers = JSON.parse(await fs.readFile('./talker.json'));
   const talker = talkers.find((element) => element.id === Number(id));
   if (!talker) {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
@@ -27,6 +29,6 @@ app.get('/talker/:id', (req, res) => {
   res.status(200).json(talker);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log('Online');
 });
