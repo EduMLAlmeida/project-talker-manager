20ba9c40688aa4a27f42c86be70671230c3349f0
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-06-28 14:18:56 -0300

    Atende requisitos 3 e 4.

--- index.js
+++ index.js
@@ -9,6 +9,51 @@ const PORT = '3000';
 
 const fs = require('fs/promises');
 
+const validateTests = (email, password) => {
+  console.log('validation', email, password);
+  const testResults = [];
+  const emailCheck = email !== '' && email !== undefined;
+  testResults.push(emailCheck);
+  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  const emailValidation = emailRegex.test(email);
+  testResults.push(emailValidation);
+  const passwordCheck = password !== '' && password !== undefined;
+  testResults.push(passwordCheck);
+  if (password === undefined) {
+    testResults.push(false);
+    return testResults;
+  }
+  const passwordValidation = password.length > 5;
+  testResults.push(passwordValidation);
+  return testResults;
+};
+
+const codeReturner = (testResults, res) => {
+  console.log(testResults);
+  if (!testResults[0]) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!testResults[1]) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!testResults[2]) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (!testResults[3]) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+};
+
+// função createToken retirada de https://www.webtutorial.com.br/funcao-para-gerar-uma-string-aleatoria-random-com-caracteres-especificos-em-javascript/
+const createToken = () => {
+  let token = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 16; i += 1) {
+      token += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return token;
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -16,7 +61,7 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (_req, res) => {
   const talkers = JSON.parse(await fs.readFile('./talker.json'));
-  res.status(200).json(talkers);
+  return res.status(200).json(talkers);
 });
 
 app.get('/talker/:id', async (req, res) => {
@@ -26,7 +71,17 @@ app.get('/talker/:id', async (req, res) => {
   if (!talker) {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
-  res.status(200).json(talker);
+  return res.status(200).json(talker);
+});
+
+app.post('/login', (req, res) => {
+  const testResults = validateTests(req.body.email, req.body.password);
+  const isValid = testResults.every((result) => result === true);
+  const token = createToken();
+  if (isValid) {
+    return res.status(200).json({ token });
+  }
+  codeReturner(testResults, res);
 });
 
 app.listen(PORT, async () => {
